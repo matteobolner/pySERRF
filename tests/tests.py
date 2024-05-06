9167fe7e2024f2fe5b4fcb634ed52ac2c5d8d389
matteobolner <matteo.bolner2@unibo.it>
2024-05-06 13:57:22 +0200

    Added tests for correlation computation

--- tests/tests.py
+++ tests/tests.py
@@ -2,6 +2,9 @@ import numpy as np
 import pandas as pd
 import pytest
 from pyserrf import SERRF
+
+# from pyserrf.serrf import read_and_parse_excel
+
 from sklearn.preprocessing import StandardScaler
 
 
@@ -54,3 +57,43 @@ class TestStandardScaler:
         )
         result = serrf._standard_scaler(data).round(2)
         pd.testing.assert_frame_equal(result, expected_result)
+
+
+
+
+class TestGetCorrsBySampleTypeAndBatch:
+    @pytest.fixture
+    def serrf_instance(self):
+        # Instantiate your SERRF class here
+        dataset = pd.read_table("tests/SERRF_example_dataset.tsv")
+        _serrf_instance = SERRF(random_state=42)
+        _serrf_instance.fit(dataset)
+        return _serrf_instance
+
+    def test_returned_dicts(self, serrf_instance):
+        corrs_qc, corrs_target = serrf_instance._get_corrs_by_sample_type_and_batch()
+        assert isinstance(corrs_qc, dict)
+        assert isinstance(corrs_target, dict)
+
+    def test_keys_correspond_to_batches(self, serrf_instance):
+        corrs_qc, corrs_target = serrf_instance._get_corrs_by_sample_type_and_batch()
+        dataset_batches = serrf_instance._dataset["batch"].unique()
+        assert set(corrs_qc.keys()) == set(dataset_batches)
+        assert set(corrs_target.keys()) == set(dataset_batches)
+
+    def test_correlations_calculation(self, serrf_instance):
+        corrs_qc, corrs_target = serrf_instance._get_corrs_by_sample_type_and_batch()
+        assert corrs_qc["A"].equals(serrf_instance._dataset[
+            serrf_instance._metabolites
+        ].loc[
+            serrf_instance._dataset[(serrf_instance._dataset["sampleType"] == "qc")&(serrf_instance._dataset["batch"] == "A")].index
+        ].corr(
+            method="spearman"
+        ))
+        assert corrs_target["A"].equals(serrf_instance._dataset[
+            serrf_instance._metabolites
+        ].loc[
+            serrf_instance._dataset[(serrf_instance._dataset["sampleType"] != "qc")&(serrf_instance._dataset["batch"] == "A")].index
+        ].corr(
+            method="spearman"
+        ))
